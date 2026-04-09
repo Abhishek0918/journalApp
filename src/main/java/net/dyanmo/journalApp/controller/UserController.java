a73c3750097ffa5a0fca9130fc43cc000bdecf6e
abhishek0918 <95862515+Abhishek0918@users.noreply.github.com>
2026-04-09 23:26:01 +0530

    created and updated users feature

---
+++ src/main/java/net/dyanmo/journalApp/controller/UserController.java
@@ -0,0 +1,43 @@
+package net.dyanmo.journalApp.controller;
+
+
+import net.dyanmo.journalApp.entity.User;
+import net.dyanmo.journalApp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAll();
+    }
+
+    @PostMapping
+    public void createUser(@RequestBody User user){
+        userService.saveEntry(user);
+    }
+
+    @PutMapping("/{userName}")
+    public ResponseEntity<User> updateUser(@RequestBody User user, @PathVariable String userName){
+        User userInDb = userService.findByUserName(user.getUsername());
+        if(userInDb != null){
+            userInDb.setUsername(user.getUsername());
+            userInDb.setPassword(user.getPassword());
+            userService.saveEntry(userInDb);
+        }
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+    }
+}
+;
---
+++ src/main/java/net/dyanmo/journalApp/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package net.dyanmo.journalApp.repository;
+
+import net.dyanmo.journalApp.entity.User;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<User, ObjectId>{
+
+    User findByUsername(String username);
+
+
+}
